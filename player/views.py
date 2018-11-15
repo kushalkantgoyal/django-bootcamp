e1d720acf936be17e2236ee3d170a0f47b512b83
Kushal Kant Goyal <kushalkantgoyal@gmail.com>
2018-11-15 12:49:14 +0530

    Binding of data to view.

--- player/views.py
+++ player/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from gameplay.models import Game
+
 
 def home(request):
-    return render(request, "player/home.html")
+    return render(request, "player/home.html",
+                  {'ngames': Game.objects.count()})
