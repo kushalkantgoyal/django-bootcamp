8bb5543d465d582aaf189c356cd6994f54c9364d
Kushal Kant Goyal <kushalkantgoyal@gmail.com>
2018-11-16 13:47:20 +0530

    Added help text.

--- player/models.py
+++ player/models.py
@@ -6,7 +6,17 @@ from django.db.models import CASCADE, PROTECT
 
 
 class Invitation(models.Model):
-    from_user = models.ForeignKey(User, related_name="invitations_sent", on_delete=PROTECT)
-    to_user = models.ForeignKey(User, related_name="invitations_received", on_delete=PROTECT)
-    message = models.CharField(max_length=300)
+    from_user = models.ForeignKey(User, related_name="invitations_sent", on_delete=models.CASCADE)
+    to_user = models.ForeignKey(
+        User,
+        related_name="invitations_received",
+        on_delete=models.CASCADE,
+        verbose_name="User to invite",
+        help_text="Please select the user to invite."
+    )
+    message = models.CharField(
+        max_length=300,
+        verbose_name="Optional message",
+        help_text="It's always nice to add a friendly message"
+    )
     timestamp = models.DateTimeField(auto_now_add=True)
