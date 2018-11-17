37cc878acf2cbffdd462c5717c1b64f2be1d6e56
Kushal Kant Goyal <kushalkantgoyal@gmail.com>
2018-11-17 20:22:01 +0530

    Added view for active and finished games.

--- player/views.py
+++ player/views.py
@@ -12,10 +12,12 @@ from player.models import Invitation
 def home(request):
     my_games = Game.objects.games_of_user(request.user)
     active_games = my_games.active()
+    finished_games = my_games.difference(active_games)
     invitations = request.user.invitations_received.all()
 
     return render(request, "player/home.html",
                   {'games': active_games,
+                   'finished_games': finished_games,
                    'invitations': invitations})
 
 @login_required
