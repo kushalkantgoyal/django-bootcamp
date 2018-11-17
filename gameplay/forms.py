2e411f88eea76defd88eb69c98550c1becbb630f
Kushal Kant Goyal <kushalkantgoyal@gmail.com>
2018-11-17 20:09:45 +0530

    Validated the move and the game is now working.

--- gameplay/forms.py
+++ gameplay/forms.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.forms import ModelForm
 
 from gameplay.models import Move
@@ -7,3 +8,14 @@ class MoveForm(ModelForm):
     class Meta:
         model=Move
         exclude = ['game', 'by_first_player']
+
+    def clean(self):
+        x = self.cleaned_data.get("x")
+        y = self.cleaned_data.get("y")
+        game = self.instance.game
+        try:
+            if game.board()[y][x] is not None:
+                raise ValidationError("Square is not empty")
+        except IndexError:
+            raise ValidationError("Invalid coordinates")
+        return self.cleaned_data
--- gameplay/urls.py
+++ gameplay/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
 
-from gameplay.views import game_detail
+from gameplay.views import game_detail, make_move
 
 urlpatterns = [
     url(r'detail/(?P<id>\d+)/$', game_detail,
-        name="gameplay_detail")
+        name="gameplay_detail"),
+
+    url(r'make_move/(?P<id>\d+)/$',
+        make_move,
+        name="gameplay_make_move")
 ]
